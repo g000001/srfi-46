35ebb88beeb59d5a832eae62e988d7bf88a47584
g000001 <chiba.masaomi@gmail.com>
2012-05-26 16:34:50 +0900

    fix: exported symbols (using cl:***)

--- package.lisp
+++ package.lisp
@@ -4,16 +4,17 @@
 
 (defpackage :srfi-46
   (:use)
+  (:import-from :cl :***)
   (:export :define-syntax :syntax-rules :let-syntax :letrec-syntax
            :***))
-;(g1:delete-package* :srfi-46.internal)
+
 (defpackage :srfi-46.internal
   (:use :srfi-46 :srfi-23 :rnrs :named-readtables :fiveam)
   (:import-from :rnrs-compat-internal
                 :define-function
                 :with-local-define-function)
   (:import-from :cl :funcall :nil)
-  (:shadowing-import-from :cl :quote)
+  (:shadowing-import-from :cl :quote :***)
   (:shadowing-import-from :srfi-46
                           :define-syntax :syntax-rules :let-syntax :letrec-syntax
                           :***))
